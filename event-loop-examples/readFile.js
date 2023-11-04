4b7c332cd01a43020a03d862db2948588ea95503
Abdul Wahab <abdulwahab@gmail.com>
2023-11-04 22:25:31 +0500

    event loop readFile

---
+++ event-loop-examples/readFile.js
@@ -0,0 +1,13 @@
+const { readFile } = require("fs");
+
+console.log("started first task");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("completed first task");
+});
+console.log("started next task");
