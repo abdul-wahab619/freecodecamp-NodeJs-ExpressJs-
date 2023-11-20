5ef36831ee67ac99860f3a682ab76907019327d2
Abdul Wahab <abdulwahab@gmail.com>
2023-11-20 19:04:23 +0500

    async pattern p1

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const { readFile } = require("fs");
-const http = require("http");
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-const server = http.createServer((req, res) => {
-  console.log("request event");
-  res.end("Hello Uni!");
-});
-
-server.listen(5000, () => {
-  console.log("Server listening on port: 5000...");
-});
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    console.log(first);
+    const first = await readFilePromise("./content/first.txt", "utf-8");
+    const second = await readFilePromise("./content/second.txt", "utf-8");
+
+    await writeFilePromise(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME: ${first} => ${second}`
+    );
+    
+    console.log(first, "=>", second);
   } catch (error) {
     console.log(error);
   }
 };
 start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
