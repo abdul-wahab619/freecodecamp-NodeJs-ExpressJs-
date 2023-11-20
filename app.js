772ec23a0a8a45948190d7690c8fcfb534231991
Abdul Wahab <abdulwahab@gmail.com>
2023-11-20 19:07:00 +0500

    async pattern p2

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const { readFile, writeFile } = require("fs");
-const util = require("util");
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFilePromise("./content/first.txt", "utf-8");
-    const second = await readFilePromise("./content/second.txt", "utf-8");
+    const first = await readFile("./content/first.txt", "utf-8");
+    const second = await readFile("./content/second.txt", "utf-8");
 
-    await writeFilePromise(
+    await writeFile(
       "./content/result-mind-grenade.txt",
-      `THIS IS AWESOME: ${first} => ${second}`
+      `THIS IS AWESOME: ${first} => ${second}`,
+      { flag: "a" }
     );
-    
+
     console.log(first, "=>", second);
   } catch (error) {
     console.log(error);
