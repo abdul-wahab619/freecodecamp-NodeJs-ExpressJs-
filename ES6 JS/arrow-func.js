adc2aef7efd8e266318eb6a46c82b25885cbe1c4
Abdul Wahab <awminhas619@gmail.com>
2024-01-07 21:41:37 +0500

    arrow-func

---
+++ ES6 JS/arrow-func.js
@@ -0,0 +1,17 @@
+// old js main 3 functions:
+// functions statement
+// functions expression
+// anonymous function
+
+// new js mn mainly 1 function
+// arrow functions:
+// basic fat arrow func
+let abc = () => {};
+// // fat arrow func with one parameter
+let xyz = (para) => {};
+b(12);
+let xyz = (num1, num2) => {};
+xyz(12, 14);
+// fat arrow func with implicite return
+let imp = () => 66;
+console.log(imp());
