4a2748bbd194651a57ed4314e1fe9c9e98e9a94c
Abdul Wahab <abdulwahab@gmail.com>
2024-03-06 20:34:09 +0500

    Handling URL's in NodeJS

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New Request Reveived!\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     res.end("Hello from Server");
   });
 });
 
 myServer.listen("8000", () => console.log("Server Started!"));
+// URL => Uniform Resource Locator
