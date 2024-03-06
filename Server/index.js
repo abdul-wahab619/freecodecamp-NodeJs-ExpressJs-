e5a700b033ddcb5f853bf5fe91a25b1fe64836cc
Abdul Wahab <abdulwahab@gmail.com>
2024-03-06 20:46:59 +0500

    HTTP methods

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} New Request Reveived!\n`;
+  const log = `${Date.now()}: ${req.url} ${req.method} New Request Reveived!\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
