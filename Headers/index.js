8932e3b17012d73417b67f23237b8a25c9b97676
Abdul Wahab <abdulwahab@gmail.com>
2024-03-07 19:33:58 +0500

    HTTP Status Codes

--- Headers/index.js
+++ Headers/index.js
@@ -100,7 +100,7 @@ app.post("/api/users", (req, res) => {
   const body = req.body;
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "pending", id: users.length + 1 });
+    return res.status(201).json({ status: "pending", id: users.length + 1 });
   });
 });
 
