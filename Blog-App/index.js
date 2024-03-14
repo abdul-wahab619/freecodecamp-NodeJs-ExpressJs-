6d8e31b22e760b33ef1538b46cd437cd25581b51
Abdul Wahab <abdulwahab@gmail.com>
2024-03-14 11:59:17 +0500

    Blogify project done

--- Blog-App/index.js
+++ Blog-App/index.js
@@ -2,7 +2,10 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog.js");
 const userRoute = require("./routes/user.js");
+const blogRoute = require("./routes/blog.js");
+
 const {
   checkForAuthenticationCookie,
 } = require("./middlewares/authentication.js");
@@ -20,12 +23,16 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({}).sort("createdAt");
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 
